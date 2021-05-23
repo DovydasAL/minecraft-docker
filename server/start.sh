1a774b5b785a7d3e3055fc9d0b889e67a782f9f2
DovydasAL <davidlazauskas1@gmail.com>
2021-05-23 17:41:30 -0400

    add java home

--- server/start.sh
+++ server/start.sh
@@ -8,4 +8,8 @@ if [[ ! -z "${pacman_packages}" ]]; then
 	pacman -S --needed $pacman_packages --noconfirm
 fi
 
+ln -fs /usr/lib/jvm/java-11-openjdk/bin/java /usr/bin/java
+archlinux-java set java-11-openjdk
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+
 java -server -Xms5G -Xmx16G -XX:MaxPermSize=128M -jar forge-1.12.2-14.23.5.2846-universal.jar
